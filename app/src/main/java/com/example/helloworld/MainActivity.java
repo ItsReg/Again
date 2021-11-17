56906c1babd0aa62b359a17b126a4a5c51f6160a
ItsReg <sandytechexpert@gmail.com>
2021-11-18 01:11:31 +0530

    ViewBinding added

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "sandy friend";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        binding.textHello.setText("View Binding use hua hia");
         Log.d(TAG, "onCreate: ");
-
-        Log.d(TAG, "Experimental branch");
+        Log.d(TAG, "from Experimental branch");
 
     }
 }
